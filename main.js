52ce36db25ff0527b3744b08a01e95aa27548767
nataliapakhomova <59440554+nataliapakhomova@users.noreply.github.com>
2020-01-02 17:24:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let myName = 'Natalya';
+console.log(`Привет, мир! Меня зовут ${myName}.`);
+let algebra = 5;
+let geogr = 4;
+let fiz = 4;
+let sroc;
+sroc = (algebra + geogr + fiz) / 3;
+console.log(sroc);
